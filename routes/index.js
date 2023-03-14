b7dd64f5a5d4417fdd5b4f9732277db73fbb592f
sirananasthefifth <emil.embretsen@elev.ga.ntig.se>
2023-03-14 15:25:00 +0100

    Final website

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.session.userid){
+    res.render('index', { title: req.session.userid });
+  } else {
+    res.redirect("/login")
+  }
 });
 
 module.exports = router;
